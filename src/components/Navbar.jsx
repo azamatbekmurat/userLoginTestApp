7b1388d57f66498a13d5c309c7e0bd9c29205119
Azamat Bekmuratov <azamat.bekmuratov24@gmail.com>
2019-04-27 08:58:31 -0700

    add components directory with Navbar and Login components. Aslo, it will make some changes on App.js

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <nav className="nav-wrapper grey darken-3">
+      <div className="container">
+        <Link to="/" className="brand=logo">
+          iLink Systems
+        </Link>
+        <ul className="right">
+          <li>
+            <NavLink to="/">About</NavLink>
+          </li>
+          <li>
+            <NavLink to="/login">Login</NavLink>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
