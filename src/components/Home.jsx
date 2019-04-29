1c058f8d176f7b7992152bbff7cb01987232f2fb
Azamat Bekmuratov <azamat.bekmuratov24@gmail.com>
2019-04-28 22:24:19 -0700

    add Home component for landing page

---
+++ src/components/Home.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="dashboard container">
+      <div className="row">
+        <div className="col s12 m9">
+          <h4>
+            Welcome to this test app where there is API call made to the
+            end-point
+          </h4>
+          <p>
+            Duis at nibh eu nulla cursus auctor fermentum sit amet mauris. Sed
+            eget interdum neque, in auctor risus. Morbi fermentum congue
+            tristique. Etiam sit amet aliquet est, vulputate feugiat orci.
+            Curabitur justo lacus, tincidunt sed sollicitudin in, tristique
+            vitae nisl. Quisque ligula nisi, tincidunt nec aliquam ut, posuere
+            in diam. Praesent convallis mauris at bibendum gravida. Vivamus dui
+            mi, sagittis eget venenatis et, porta sed ex
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
