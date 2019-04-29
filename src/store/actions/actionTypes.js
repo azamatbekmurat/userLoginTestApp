3d4be95944faa9908380607c349db960365deabc
Azamat Bekmuratov <azamat.bekmuratov24@gmail.com>
2019-04-28 22:22:46 -0700

    add action creator with method to make API Call

--- src/store/actions/actionTypes.js
+++ src/store/actions/actionTypes.js
@@ -1,2 +1,20 @@
-export const AUTH_LOGIN = "AUTH_LOGIN";
-export const AUTH_LOGOUT = "AUTH_LOGOUT";
+// Action creator where with fetch method handling API call and dispatching received data of users
+export function fetchUsers() {
+  return dispatch => {
+    // handling API call
+    return fetch("http://localhost:61080/api/users")
+      .then(res => res.json())
+      .then(json => {
+        let users = json;
+        //dispatching users
+        dispatch(fetchUsersSuccess(users));
+      });
+  };
+}
+
+export const FETCH_USERS_SUCCESS = "FETCH_USERS_SUCCESS";
+
+export const fetchUsersSuccess = users => ({
+  type: FETCH_USERS_SUCCESS,
+  payload: { users }
+});
